c422e966ae0e85812ce9825b01e8d8a6b63720ce
ifiddes <ian.t.fiddes@gmail.com>
2014-11-02 11:40:24 -0800

    forgot xlim

--- component_histogram.R
+++ component_histogram.R
@@ -6,8 +6,8 @@ data <- read.table(args[1], row.names=1)
 
 pdf(args[2])
 
-hist(data, main="Sizes of weakly connected components", xlab="size (bp)")
+hist(data[,1], main="Sizes of weakly connected components", xlab="size (bp)")
 
-hist(data, main='Sizes of WCCs - up to 100', xlab="size (bp)", breaks="FD")
+hist(data[,1], main='Sizes of WCCs - up to 100', xlab="size (bp)", breaks="FD", xlim=c(0,100))
 
 dev.off()
